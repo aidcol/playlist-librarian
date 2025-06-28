eda985c542acdcf842c72dc3cce508fbb017e84d
aidcol <aidancolon20@gmail.com>
2025-06-27 18:06:54 -0600

    Test spotify client using main.py
    Implemented a simple test of the authentication and API access for
    the SpotifyClientManager in main.py.

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from src.config import Config, ConfigError
+from src.spotify_client import SpotifyClientManager, SpotifyClientError, SpotifyException
 
 def main():
     print("Hello from playlist-librarian!")
 
     try:
         config = Config()
-        print(f'{config.spotify_client_id=}')
-        print(f'{config.spotify_client_secret=}')
     except ConfigError as e:
         print(e)
 
+    client_manager = SpotifyClientManager(config.spotify_client_id,
+                                          config.spotify_client_secret)
+    
+    if not client_manager.authenticate():
+        exit(1)
+    
+    if not client_manager.test_connection():
+        exit(1)
 
 if __name__ == "__main__":
     main()
